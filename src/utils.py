728842eeb569dca0c2c60e96e134e175694e2f05
Oheir <lgfanton@me.com>
2023-02-07 15:45:55 +0000

    fin d'heure

--- src/utils.py
+++ src/utils.py
@@ -12,12 +12,12 @@ img = [
 ]
 
 def img2ascii(img, black= '#' , white= '.'):
-    result = ''
+    result = []
     for x in img:
         coordin = [black if y == 1 else white for y in x]   
         print(coordin) 
-        result = ''.join(coordin[0])  
-    return result 
+        result.append(''.join([black if y == 1 else white for y in x]))
+    return '\n'.join(result) 
 
 
 ascii = img2ascii(img, 'O', '.')
